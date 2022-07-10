979a86696c4c9f02e14fe54d635cd4208a134f21
George Jalaghonia <gio@MacBook-Pro.local>
2022-07-10 16:35:49 +0400

    admin section done. add new user user edit page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -4,6 +4,7 @@ Test for the Django admin modifications.
 """
 
 
+from ast import arg
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -34,3 +35,17 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
